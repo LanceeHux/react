65599f62fe88626fbaf8d9dc50e6f65d20a98dd7
LeeHux <laziolynx@gmail.com>
2026-07-04 01:22:43 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
+    <>
     <Navbar />
     <div>
       <h1>Hello React!</h1>
       <p>My first React app</p>
     </div>
+    </>
   );
 }
