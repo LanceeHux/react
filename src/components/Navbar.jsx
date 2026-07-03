e157271cd8e503c261042ed717088fccd2417e0e
LeeHux <laziolynx@gmail.com>
2026-07-04 01:18:35 +0800

    Add Navbar component with navigation links

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <h2>Lanx</h2>
+
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Projects</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+  );
+}
