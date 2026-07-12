6c96b750e83c620511c9d275796b25633d695d39
LeeHux <laziolynx@gmail.com>
2026-07-12 19:43:22 +0800

    Create lesson 3/App.jsx

---
+++ lessons/lesson 3/App.jsx
@@ -0,0 +1,11 @@
+import React from "React";
+import Profile from "./Profile";
+
+export default function App() {
+  return (
+    <>
+      <Profile name="Lanx" age={17} />
+      <Profile name="Alex" age={20} />
+    </>
+  );
+}
