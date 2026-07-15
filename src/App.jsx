94998ad43495f28fdff8e0da115261aad9a0d0db
LeeHux <laziolynx@gmail.com>
2026-07-15 11:23:38 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ export default function App() {
     </header>
     <main className="w-full bg-black min-h-screen text-white">
       <section className="min-h-screen w-full flex flex-row justify-center items-center gap-10 bg-[radial-gradient(circle_at_top_left,_#1c1c1c,_transparent_70%)]">
-        <div className="flex flex-col gap-5">
+        <div className="flex flex-col gap-5 md:flex-row md:gap-5">
           <h1 className='text-8xl'>My React Site!</h1>
           <p className=''>This is my first React Site! </p>
           <div className="flex gap-[60%]">
