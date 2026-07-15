1717730df550409c5883f1ddca9fb13be7f547b4
LeeHux <laziolynx@gmail.com>
2026-07-15 11:28:07 +0000

    fixed tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ export default function App() {
     </header>
     <main className="w-full bg-black min-h-screen text-white">
       <section className="min-h-screen w-full flex flex-col justify-center items-center gap-10 bg-[radial-gradient(circle_at_top_left,_#1c1c1c,_transparent_70%)] md:flex-row">
-        <div className="flex flex-col gap-5 md:flex-row md:gap-5">
+        <div className="flex flex-col gap-5">
           <h1 className='text-8xl'>My React Site!</h1>
           <p className=''>This is my first React Site! </p>
           <div className="flex gap-[60%]">
@@ -21,7 +21,7 @@ export default function App() {
             <button className='[padding:5px_15px_5px_15px] rounded-xl [letter-spacing:3px] bg-[#ffcd00] text-black hover:bg-white text-black duration-300'>Github</button>
           </div>
         </div>
-        <div className="p-[30px] bg-[#1c1c1c] rounded-3xl [margin-top:7%]">
+        <div className="p-[30px] bg-[#1c1c1c] rounded-3xl [margin-top:0%]">
           <img src="https://lanceehux.github.io/images/profile.jpg" className="w-[400px] h-[500px] rounded-3xl [border:2px_solid_white]"/>
           <p className="px-4 py-2 mt-4 text-gray-300 bg-white/5 border border-white/10 rounded-lg font-medium tracking-wide">
           Likes: <span className="text-white font-bold">Likes: {likes}</span>
