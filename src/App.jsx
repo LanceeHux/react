e0e219156fd70f763c3ce4b2bed0c359f4edd91e
LanceeHux <laziolynx@gmail.com>
2026-09-24 02:43:35 +0800

    section build [incomplete]

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,12 @@ export default function App() {
     header: "p-5 shadow-xl flex flex-row justify-between",
     headerTitle: "text-xl [font-weight:bold]",
     headerBtns: "flex gap-3",
-    headerBtn: "hover:bg-black hover:text-white [transition:0.3s_ease] p-2 rounded-sm"
+    headerBtn: "hover:bg-black hover:text-white [transition:0.3s_ease] p-2 rounded-sm",
+    section: "min-h-screen justify-center items-center flex",
+    itemList: "p-3 flex gap-2 shadow-xl",
+    itemCard: "p-2 rounded-md flex flex-col shadow-md",
+    itemPrice: "text-[#ffcd00]/90 [font-weight:bold]",
+    itemDesc: "text-[60%]"
   }
   return (
     <>
@@ -17,6 +22,34 @@ export default function App() {
         <a href="#" className={styles.headerBtn}>Profile</a>
       </div>
     </header>
+
+    <section className={styles.section}>
+      <div>
+        <h2 className={styles.headerTitle}>Top Item Sales / Popular</h2>
+        <div className={styles.itemList}>
+        <div className={styles.itemCard}>
+            <span>Keychain Dog</span>
+            <code className={styles.itemPrice}>$9.81</code>
+            <p className={styles.itemDesc}>A cute fancy keychain for everyone, for bag, unisex.</p>
+          </div>
+          <div className={styles.itemCard}>
+            <span>Keychain Dog</span>
+            <code className={styles.itemPrice}>$9.81</code>
+            <p className={styles.itemDesc}>A cute fancy keychain for everyone, for bag, unisex.</p>
+          </div>
+          <div className={styles.itemCard}>
+            <span>Keychain Dog</span>
+            <code className={styles.itemPrice}>$9.81</code>
+            <p className={styles.itemDesc}>A cute fancy keychain for everyone, for bag, unisex.</p>
+          </div>
+          <div className={styles.itemCard}>
+            <span>Keychain Dog</span>
+            <code className={styles.itemPrice}>$9.81</code>
+            <p className={styles.itemDesc}>A cute fancy keychain for everyone, for bag, unisex.</p>
+          </div>
+        </div>
+      </div>
+    </section>
     </>
   )
 }
