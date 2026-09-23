d0a5643136ca659a218e3d1b24b7dc283ec081a5
LanceeHux <laziolynx@gmail.com>
2026-09-24 02:29:14 +0800

    header part done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import { useState, useEffect } from "react"
 
 export default function App() {
+  const styles = {
+    header: "p-5 shadow-xl flex flex-row justify-between",
+    headerTitle: "text-xl [font-weight:bold]",
+    headerBtns: "flex gap-3",
+    headerBtn: "hover:bg-black hover:text-white [transition:0.3s_ease] p-2 rounded-sm"
+  }
   return (
     <>
-    <header class="shadow-xl">
-      <h1>Lezada</h1>
-      <div>
-        <a href="#">Dashboard</a>
-        <a href="#">My Cfart</a>
-        <a href="#">Profile</a>
+    <header className={styles.header}>
+      <h1 className={styles.headerTitle}>Lezada</h1>
+      <div className={styles.headerBtns}>
+        <a href="#" className={styles.headerBtn}>Dashboard</a>
+        <a href="#" className={styles.headerBtn}>My Cart</a>
+        <a href="#" className={styles.headerBtn}>Profile</a>
       </div>
     </header>
     </>
