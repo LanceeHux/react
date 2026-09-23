ee936dddba7d79488d3d307829ecfe0cced3fcac
LanceeHux <laziolynx@gmail.com>
2026-09-24 01:55:50 +0800

    header built

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,14 @@ import { useState, useEffect } from "react"
 export default function App() {
   return (
     <>
-    hello
+    <header>
+      <h1>Lezada</h1>
+      <div>
+        <a href="#">Dashboard</a>
+        <a href="#">My Cart</a>
+        <a href="#">Profile</a>
+      </div>
+    </header>
     </>
   )
 }
